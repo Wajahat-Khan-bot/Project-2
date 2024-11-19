687a6de59572788d15f9ec69e096d25113e3f585
Wajahat Khan <wajahatkhan@gmail.com>
2024-11-19 17:13:41 +0500

    code Updated

--- P2-Main-File.py
+++ P2-Main-File.py
@@ -14,40 +14,53 @@ while True:
 print("Ok Let's Play")
 
 score = 0
+correct = 0
+incorrect = 0
 
 # Question 1
-print("What is the capital city of Canada?")
+print("1) What is the capital city of Canada?")
 answer = input("(a) Toronto b) Vancouver c) Ottawa d) Montreal) \nAnswer:").lower()
 
 if answer == "ottawa" or answer == "c":
     print("Correct!")
     score += 2
+    correct += 1
 else:
     print("Incorrect")
     score -= 0.5
+    incorrect += 1
 
 
 # Question 2
-print("What is the chemical symbol for gold?")
+print("2) What is the chemical symbol for gold?")
 answer = input("a) Au  b) Ag  c) Pb  d) Fe \nAnswer:").lower()
-if answer == "Au" or answer == "a":
+if answer == "au" or answer == "a":
     print("Correct!")
     score += 2
+    correct += 1
 
 else:
     print("Incorrect!")
     score -= 0.5
+    incorrect += 1
 
 # Question 3
 print("Who was the first president of the United States?")
 answer = input(
     "a) Thomas Jefferson b) Benjamin Franklin c) George Washington d) Abraham Lincoln \nAnswer:").lower()
-if answer == "c) George Washington" or answer == "c":
+if answer == "george washington" or answer == "c":
     print("Correct!")
     score += 2
+    correct += 1
 
 else:
     print("Incorrect!")
     score -= 0.5
+    incorrect += 1
 
+
+print("--------------------------")
+print("You answer", correct, "correct answers.")
+print("You answer", incorrect, "incorrect answers.")
+print("After negative marking of 0.5/incorrect answer")
 print("Your final score is ",score)
